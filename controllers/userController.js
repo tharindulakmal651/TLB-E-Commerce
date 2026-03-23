8681b3007bdbb317b03856c622b0c619e13b38c5
tharindulakmal651 <155797706+tharindulakmal651@users.noreply.github.com>
2026-03-24 00:13:35 +0530

    create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+import User from "../models/user.js";
+
+export function createUser(req, res) {
+  
+   const user = new User(req.body)
+    user.save().then(() => {
+        res.json({
+            message: "User created successfully !!!"
+        })
+    }).catch(() => {
+        res.json({
+            message: "Error creating User !!!"      
+        })
+
+    })
+}
